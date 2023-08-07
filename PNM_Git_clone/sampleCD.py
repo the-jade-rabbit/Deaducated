c20ebb7b8a3a73b189cad7ad4949fa6710730cfa
the-jade-rabbit <jaa.nnamdi@icloud.com>
2023-08-07 16:30:22 +0000

    fixed the errors...carry on

--- PNM_Git_clone/sampleCD.py
+++ PNM_Git_clone/sampleCD.py
@@ -4,16 +4,14 @@
 import random
 ##
 
+
 class SampleData():
     '''initialise core data members as random values'''
-    def __init__(self, direction = 'left/right') -> None:
+
+    def __init__(self) -> None:
         self.CoreData = {'ts_start': [random.randint(3, 99)],
-                    'ts_end': [random.randint(3, 99)],
-                    'no_of_bits': [random.randint(1096, 512*8)],
-                        }
-        directionLR = direction 
-        
-    @property
-    def checkDirect(self):
-        if directionLR == '':
-            raise
+                         'ts_end': [random.randint(3, 99)],
+                         'no_of_bits': [random.randint(1096, 512*8)],
+                         }
+        direction = ["right", "left"]
+        self.directionLR = random.choice(direction)
